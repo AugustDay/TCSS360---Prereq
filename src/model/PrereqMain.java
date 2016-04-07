a09c27f0fe458ba3ca5b6acc3549cd168698abf3
Austin Ingraham <adi1996@uw.edu>
2016-04-07 09:48:35 -0700

    Test commit, added main

---
+++ src/model/PrereqMain.java
@@ -0,0 +1,22 @@
+/* TCSS 360 
+ * Prerequisite Graph
+ * main.java
+ */
+
+package model;
+
+/**
+ * main class launches program.
+ * @author Austin Ingraham (adi1996@uw.edu)
+ * @version 4-7-2016
+ */
+public final class PrereqMain {
+
+    /**
+     * main method launches program.
+     * @param theArgs command line input
+     */
+    public static void main(final String[] theArgs) {
+        System.out.println("Hello world!");
+    }
+}
